6b380c7c9ebf4438b00299e39e36fe1f9dbde35e
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-17 16:19:46 +0300

    h

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  base: "/my-app/",
-});
+})
